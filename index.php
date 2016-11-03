e1560116f4743daebec6ddc687cbc46ca8752f25
Kreatf Code <kreatf.media@gmail.com>
2016-11-03 09:56:28 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,11 +7,31 @@
       Cek Service Status : <span></span>
     </div>
     <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
-    <script type="javascript" src="jquery.soap.js"></script>
-    <script type="javascript">
-      $(function() {
-        alert("OK");
-      });
+    <script src="jquery.soap.js"></script>
+    <script>
+      $.soap({
+    url: 'https://esbbcext01.beacukai.go.id:9082/penerimaandatasvc/WSTPB',
+    method: 'getKurs',
+
+    data: {
+        kodeValuta: 'USD',
+        tglValuta: '2016-11-02'
+    },
+
+    success: function (soapResponse) {
+        // do stuff with soapResponse
+        // if you want to have the response as JSON use soapResponse.toJSON();
+        // or soapResponse.toString() to get XML string
+        // or soapResponse.toXML() to get XML DOM
+      console.log('success');
+      console.log(soapResponse);
+    },
+    error: function (SOAPResponse) {
+        // show error
+      console.log('error');
+      console.log(soapResponse);
+    }
+});
     </script>
   </body>
 </html>
