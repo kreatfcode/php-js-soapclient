c5a7083ed0981d340e93b60b5b910e001a005284
Kreatf Code <kreatf.media@gmail.com>
2016-11-03 09:51:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
       Cek Service Status : <span></span>
     </div>
     <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
-    <script type="javascript" src="https://raw.githubusercontent.com/doedje/jquery.soap/master/jquery.soap.js"></script>
+    <script type="javascript" src="jquery.soap.js"></script>
     <script type="javascript">
       $(function() {
         alert("OK");
